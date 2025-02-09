dd05ec43d3e56354e42b0823768bf4856a9ab7ef
DaniilQAspb <d.korobejnikov2016@yandex.ru>
2025-02-09 23:39:03 +0300

    Исправлена миграци

--- backend_bd/migrations/2025020901-add-price-column-to-cars.js
+++ backend_bd/migrations/2025020901-add-price-column-to-cars.js
@@ -1,15 +1,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Проверка наличия столбца 'price' с использованием прямого SQL-запроса
+    // Проверка на существование таблицы 'cars'
     const result = await queryInterface.sequelize.query(
-      `SELECT column_name 
-         FROM information_schema.columns 
-         WHERE table_schema = 'public' 
-         AND table_name = 'cars' 
-         AND column_name = 'price';`
+      `SELECT to_regclass('public.cars');`
     );
 
-    if (result[0].length === 0) {
+    if (!result[0][0].to_regclass) {
+      throw new Error("Таблица 'cars' не существует!");
+    }
+
+    // Проверка наличия столбца 'price' с использованием прямого SQL-запроса
+    const columnResult = await queryInterface.sequelize.query(
+      `SELECT column_name 
+           FROM information_schema.columns 
+           WHERE table_schema = 'public' 
+           AND table_name = 'cars' 
+           AND column_name = 'price';`
+    );
+
+    if (columnResult[0].length === 0) {
       await queryInterface.addColumn("cars", "price", {
         type: Sequelize.DECIMAL(10, 2),
         allowNull: true,
