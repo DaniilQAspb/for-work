54235d53340147657454d8f1c99506b8391bf0a7
DaniilQAspb <d.korobejnikov2016@yandex.ru>
2025-02-09 21:41:02 +0300

    Добавлен ci.yml для гитхаба, а гитлабовский удален

--- backend_bd/db_test.js
+++ backend_bd/db_test.js
@@ -3,18 +3,15 @@ const { Pool } = require("pg");
 console.log("Текущий NODE_ENV:", process.env.NODE_ENV); // Логируем текущую среду
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database:
-    process.env.NODE_ENV === "test" ? "CarDealership_test" : "CarDealership", // Проверка среды
-  password: "root",
-  port: 5432,
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "127.0.0.1", // Для GitHub Actions
+  database: process.env.NODE_ENV === "test" ? "testdb" : "CarDealership",
+  password: process.env.DB_PASSWORD || "root",
+  port: process.env.DB_PORT || 5432,
 });
 
-console.log(
-  "Используемая база данных:",
-  process.env.NODE_ENV === "test" ? "CarDealership_test" : "CarDealership"
-);
+console.log("Используемая база данных:", pool.options.database);
+
 // Логируем текущую базу данных
 pool.query("SELECT current_database()", (err, res) => {
   if (err) {
