a730444588f1c7e017cd77d27518e85fa6452e9d
DaniilQAspb <d.korobejnikov2016@yandex.ru>
2025-02-09 18:20:06 +0300

    Добавлена тестовая БД и на ней теперь проводятся тесты

--- backend_bd/db_test.js
+++ backend_bd/db_test.js
@@ -1,12 +1,20 @@
-// db_test.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "CarDealership_test", // Используйте другую базу данных для тестов
+  database: "CarDealership_test", // Пробуем основную базу данных
   password: "root",
   port: 5432,
+});
+
+// Логируем текущую базу данных
+pool.query("SELECT current_database()", (err, res) => {
+  if (err) {
+    console.error("Ошибка при подключении к базе данных:", err);
+  } else {
+    console.log("Текущая база данных:", res.rows[0].current_database);
+  }
 });
 
 module.exports = { pool };
