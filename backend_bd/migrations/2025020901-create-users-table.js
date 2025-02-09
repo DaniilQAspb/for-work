53a9cec7ed2d0d496cf2c122b372659b5336c112
DaniilQAspb <d.korobejnikov2016@yandex.ru>
2025-02-10 00:32:51 +0300

    Изменена миграция

---
+++ backend_bd/migrations/2025020901-create-users-table.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      user_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    console.log("Таблица 'users' была создана.");
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+    console.log("Таблица 'users' была удалена.");
+  },
+};
